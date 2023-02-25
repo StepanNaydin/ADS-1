0485815aedaa9320284a02b55d090a3dd4c5cc71
StepanNaydin <124874525+StepanNaydin@users.noreply.github.com>
2023-02-25 18:41:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,26 +7,28 @@ bool checkPrime(uint64_t value) {
 	bool x = true;
 	if (value == 2)
 		return x;
-	for (int q = 2; q < sqrt(value)+1;q++) {
-		if (value%q==0)
+	for (int q = 2; q < sqrt(value) + 1; q++) {
+		if (value % q == 0){
 			x = false;
+		}
 	}
 	return x;
 }
 
 uint64_t nPrime(uint64_t n) {
-	int c = 1,shet=0;
+	int c = 1, shet = 0;
 	while (true) {
 		bool x = true;
 		c++;
 		if (c != 2) {
-			for (int q = 2; q < sqrt(c) + 1;q++) {
-				if (c % q == 0)
+			for (int q = 2; q < sqrt(c) + 1; q++) {
+				if (c % q == 0){
 					x = false;
+				}
 			}
 		}
 		//std::cout << x;
-		if (x==true)
+		if (x == true)
 			shet++;
 		if (shet == n)
 			return c;
@@ -40,12 +42,14 @@ uint64_t nextPrime(uint64_t value) {
 	while (true) {
 		bool x = true;
 		c++;
-		for (int q = 2; q < sqrt(c)+1;q++) {
-			if (c % q == 0)
+		for (int q = 2; q < sqrt(c) + 1; q++) {
+			if (c % q == 0){
 				x = false;
+			}
 		}
-		if (x==true)
+		if (x == true){
 			return c;
+		}
 	}
 }
 
@@ -53,14 +57,16 @@ uint64_t sumPrime(uint64_t hbound) {
 	int sum = 2;
 	if (hbound < 3)
 		return 0;
-	for (int q = 2; q < hbound;q++) {
+	for (int q = 2; q < hbound; q++) {
 		bool x = true;
-		for (int w = 2; w < sqrt(q) + 1;w++) {
-			if (q % w == 0)
+		for (int w = 2; w < sqrt(q) + 1; w++) {
+			if (q % w == 0){
 				x = false;
+			}
 		}
-		if (x == true)
-			sum+=q;
+		if (x == true){
+			sum += q;
+		}
 		//std::cout << q << " " << sum << std::endl;
 	}
 	return sum;
