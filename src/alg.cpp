a24bf156731da8ff6b82c69a7cc2f7107b90c89c
StepanNaydin <124874525+StepanNaydin@users.noreply.github.com>
2023-02-25 18:24:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,64 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	bool x = true;
+	if (value == 2)
+		return x;
+	for (int q = 2; q < sqrt(value)+1;q++) {
+		if (value%q==0)
+			x = false;
+	}
+	return x;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	int c = 1,shet=0;
+	while (true) {
+		bool x = true;
+		c++;
+		if (c != 2) {
+			for (int q = 2; q < sqrt(c) + 1;q++) {
+				if (c % q == 0)
+					x = false;
+			}
+		}
+		//std::cout << x;
+		if (x==true)
+			shet++;
+		if (shet == n)
+			return c;
+	}
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	int c = value;
+	if (value < 2)
+		return 2;
+	while (true) {
+		bool x = true;
+		c++;
+		for (int q = 2; q < sqrt(c)+1;q++) {
+			if (c % q == 0)
+				x = false;
+		}
+		if (x==true)
+			return c;
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	int sum = 2;
+	if (hbound < 3)
+		return 0;
+	for (int q = 2; q < hbound;q++) {
+		bool x = true;
+		for (int w = 2; w < sqrt(q) + 1;w++) {
+			if (q % w == 0)
+				x = false;
+		}
+		if (x == true)
+			sum+=q;
+		//std::cout << q << " " << sum << std::endl;
+	}
+	return sum;
 }
